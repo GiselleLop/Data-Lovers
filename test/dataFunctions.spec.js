31fe85c4824f570323905d8969401df04566c1d3
GiselleLop <giselleLop@users.noreply.github.com>
2023-10-04 10:24:46 -0500

    Arreglo actualizado

--- test/dataFunctions.spec.js
+++ test/dataFunctions.spec.js
@@ -1,8 +1,8 @@
-import { filterData, sortData } from '../src/dataFunctions.js';
+import { filterData } from '../src/dataFunctions.js';
 import { data as fakeData } from './data.js';
 
-console.log(fakeData);
-
+//console.log(fakeData);
+/*
 describe('example', () => {
 
   it('returns `example`', () => {
@@ -10,11 +10,19 @@ describe('example', () => {
   });
 });
 
-
-
 describe('anotherExample', () => {
 
   it('returns `anotherExample`', () => {
     expect(anotherExample()).toBe('OMG');
   });
+});*/
+//////////
+describe('filter data', () => {
+  const filtros = filterData(fakeData, "status", "AlL");
+  expect(filtros).toBe(fakeData);
+
+/*
+  it('returns `example`', () => {
+    expect(example()).toBe('example');
+  });*/
 });
