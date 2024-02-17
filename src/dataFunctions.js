db45255a17d2e340b0a5b46323d22853883e1a44
GiselleLop <giselleLop@users.noreply.github.com>
2024-02-16 21:42:39 -0500

    refactorizacion de codigo, arreglos css

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,22 +1,20 @@
-import { renderItems } from "./view.js";
-
 export const sortData = (data, orderTypeValue, ordenbyValue) => {
   
-  if (ordenbyValue === "id") {
+  if (ordenbyValue === "Id") {
     data.sort((a, b) => {
-      if (orderTypeValue === "asc") {
+      if (orderTypeValue === "ASC") {
         return a.id - b.id;
-      } else if (orderTypeValue === "desc") {
+      } else if (orderTypeValue === "DESC") {
         return b.id - a.id;
       }
     });
   }
-  if (ordenbyValue === "name") {
+  if (ordenbyValue === "Name") {
     data.sort((a, b) => {
       const valorA = a.name.toLowerCase();
       const valorB = b.name.toLowerCase();
       const compare = valorA.localeCompare(valorB);
-      return orderTypeValue === "asc" ? compare : -compare;
+      return orderTypeValue === "ASC" ? compare : -compare;
     });
   }
   data.sort((a, b) => {
@@ -27,8 +25,6 @@ export const sortData = (data, orderTypeValue, ordenbyValue) => {
   return data;
 };
 
-const tarjetasContainer = document.querySelector("#list_card");
-
 export const filterData = (data, filterBy, value) => {
   if (value === "All") {
     return data;
@@ -38,9 +34,9 @@ export const filterData = (data, filterBy, value) => {
 };
 
 export function filterAll(data, statusValue, speciesValue, genderValue, orderTypeValue, ordenbyValue) {
-  tarjetasContainer.innerHTML = "";
   let arrFiltered = data;
   sortData(arrFiltered, orderTypeValue, ordenbyValue);
+
   if (speciesValue !== "All") {
     arrFiltered = filterData(arrFiltered, "species", speciesValue);
   }
@@ -50,114 +46,34 @@ export function filterAll(data, statusValue, speciesValue, genderValue, orderTyp
   if (genderValue !== "All") {
     arrFiltered = filterData(arrFiltered, "gender", genderValue);
   }
-  renderItems(arrFiltered);
+  return arrFiltered
 }
 
-export const filter = (ArrayData) => {
-  //FILTRO DE ESTATUS
+export const renderFilters = (ArrayData) => {
   const listStatus = ArrayData.map((x) => x.status);
-  const statusUnicos = listStatus.filter((valor, indice) => {
+  const listStatusUniques =  listStatus.filter((valor, indice) => {
     return listStatus.indexOf(valor) === indice;
   });
-
-  const selectStatus = document.getElementById("status");
-  const dataInfo = document.createElement("option");
-  dataInfo.value = "All";
-  dataInfo.text = "All";
-  selectStatus.appendChild(dataInfo);
-  statusUnicos.forEach((unic) => {
-    const newOpcion = document.createElement("option");
-    newOpcion.value = unic;
-    newOpcion.text = unic;
-    selectStatus.appendChild(newOpcion);
+  const listSpecies = ArrayData.map((x) => x.species);
+  const listSpeciesUniques = listSpecies.filter((valor, indice) => {
+    return listSpecies.indexOf(valor) === indice;
+  });
+  const listGenders = ArrayData.map((x) => x.gender);
+  const listGendersUniques = listGenders.filter((valor, indice) => {
+    return listGenders.indexOf(valor) === indice;
   });
 
-  //FILTROS POR ESPECIES
-  const listPecies = ArrayData.map((x) => x.species);
-  const unicos = listPecies.filter((valor, indice) => {
-    return listPecies.indexOf(valor) === indice;
-  });
-
-  const selectSpecias = document.getElementById("species");
-  const dataInfoSpecie = document.createElement("option");
-  dataInfoSpecie.value = "All";
-  dataInfoSpecie.text = "All";
-
-  selectSpecias.appendChild(dataInfoSpecie);
-  
-  unicos.forEach((unic) => {
-    const newOpcion = document.createElement("option");
-    newOpcion.value = unic;
-    newOpcion.text = unic;
-
-    selectSpecias.appendChild(newOpcion);
-  });
-
-  //Filtro por Genero
-  const listGenere = ArrayData.map((x) => x.gender);
-  const genderUnics = listGenere.filter((valor, indice) => {
-    return listGenere.indexOf(valor) === indice;
-  });
-
-  const selectGender = document.getElementById("gender");
-  const dataInfoGender = document.createElement("option");
-  dataInfoGender.value = "All";
-  dataInfoGender.text = "All";
-  selectGender.appendChild(dataInfoGender);
-  genderUnics.forEach((unic) => {
-    const newOpcion = document.createElement("option");
-    newOpcion.value = unic;
-    newOpcion.text = unic;
-
-    selectGender.appendChild(newOpcion);
-  });
-
-  // filtros Ascendente y desendente
-  const data0 = document.createElement("option");
-  data0.label = "Select";
-
-  const selectOrderBy = document.querySelector("select[name='sort-order']");
-  const data1 = document.createElement("option");
-  data1.value = "asc";
-  data1.text = "ASC";
-
-  const data2 = document.createElement("option");
-  data2.value = "desc";
-  data2.text = "DESC";
-
-  selectOrderBy.appendChild(data0);
-  selectOrderBy.appendChild(data1);
-  selectOrderBy.appendChild(data2);
-
-  // filtros Ascendente y desendente
-  const databy0 = document.createElement("option");
-  databy0.label = "Select";
-
-  const selectOrderTypeBy = document.querySelector("select[name='By']");
-  const databy1 = document.createElement("option");
-  databy1.value = "id";
-  databy1.text = "ID";
-
-  const databy2 = document.createElement("option");
-  databy2.value = "name";
-  databy2.text = "Name";
-
-  selectOrderTypeBy.appendChild(databy0);
-  selectOrderTypeBy.appendChild(databy1);
-  selectOrderTypeBy.appendChild(databy2);
-};
+  const AllFilters = [ listStatusUniques, listSpeciesUniques, listGendersUniques]
+  return AllFilters
+}
 
 export const computeStats = (arrayData)=>{
-
-
-  //GRAFICO
-  const grafica = document.querySelector("#graficaGender");
+  const graficGender = document.querySelector("#graficGender");
   const arrayFemale = filterData(arrayData,"gender","Female").length
   const arrayMale = filterData(arrayData,"gender","Male").length
   const arrayUnknown = filterData(arrayData,"gender","unknown").length
-
   // eslint-disable-next-line no-undef
-  new Chart(grafica,{
+  new Chart(graficGender,{
     type: 'doughnut',
     data: {
       labels:["Female","Male","Unknown"], 
@@ -173,16 +89,12 @@ export const computeStats = (arrayData)=>{
     }
   })
 
-
-  // GRAFICO 2
-
-  const grafica2 = document.querySelector("#grafica2");
+  const graficStatus = document.querySelector("#graficStatus");
   const alive = filterData(arrayData,"status","Alive").length
   const dead = filterData(arrayData,"status","Dead").length
   const knowliv = filterData(arrayData,"status","unknown").length
-
   // eslint-disable-next-line no-undef
-  new Chart(grafica2,{
+  new Chart(graficStatus,{
     type: 'doughnut',
     data: {
       labels:["Alive","Dead","Unknown"], 
@@ -198,9 +110,7 @@ export const computeStats = (arrayData)=>{
     }
   })
 
-  // GRAFICO 3
-
-  const grafica3 = document.querySelector("#grafica3");
+  const graficSpecies = document.querySelector("#graficSpecies");
   const human = filterData(arrayData,"species","Human").length;
   const alien = filterData(arrayData,"species","Alien").length;
   const humanoid = filterData(arrayData,"species","Humanoid").length;
@@ -214,9 +124,8 @@ export const computeStats = (arrayData)=>{
   const disease = filterData(arrayData,"species","Disease").length;
   const parasite = filterData(arrayData,"species","Parasite").length;
 
-
   // eslint-disable-next-line no-undef
-  new Chart(grafica3,{
+  new Chart(graficSpecies,{
     type: 'doughnut',
     data: {
       labels:["Human","Alien","Humanoid","Unknown","Poopybutthole","Mytholog","Animal","Vampire","Robot","cronenberg","Disease","parasite"], 
---
+++ src/stadisticModal.js
@@ -0,0 +1,28 @@
+export function createModal() {
+  const modalSection = document.createElement("section");
+  modalSection.classList.add("modal");
+
+  const modalContent = `
+      <div class="header_modal">
+        <h2 class=titleModal>Statistical data</h2>
+        <button class="modal_close">Close Modal</button>
+      </div>
+      <div class="charts_container">
+        <div class="charts">
+          <p class="titleChar">In this graph we can see the number of characters divided by gender.</p>
+          <canvas id="graficGender" width="200" height="200"></canvas>
+        </div>
+        <div class="charts">
+          <p class="titleChar">In this graph we can see the number of characters divided by life status.</p>
+          <canvas id="graficStatus" width="300" height="300"></canvas>
+        </div>
+        <div class="charts">
+          <p class="titleChar">In this graph we can see the number of characters divided by species</p>
+          <canvas id="graficSpecies" width="300" height="300"></canvas>
+        </div>
+      </div>
+    `;
+
+  modalSection.innerHTML = modalContent;
+  return modalSection;
+}
